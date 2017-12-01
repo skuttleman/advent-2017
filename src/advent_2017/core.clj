ef06d47dac864524042841ed1e80ebb7bde9b120
Ben Allred <skuttleman@gmail.com>
2017-12-01 13:50:48 -0700

    Day 1 Step 2

--- src/advent_2017/core.clj
+++ src/advent_2017/core.clj
@@ -3,7 +3,7 @@
               [clojure.string :as s]))
 
 (def steps
-    [[day-1/step-1]])
+    [[day-1/step-1 day-1/step-2]])
 
 (defn -main [& [day step]]
     (let [[day' step'] (map (fn [v] (-> (str "0" v)
--- src/advent_2017/day_1.clj
+++ src/advent_2017/day_1.clj
@@ -2,16 +2,25 @@
     (:require [advent-2017.utils.core :as u]
               [clojure.string :as s]))
 
-;; 1228
-(defn step-1 []
+(defn ^:private count-em [digits-fn]
     (let [digits (->> (u/read-file 1 #"")
                      (map s/trim)
                      (remove empty?)
                      (map #(Integer/parseInt %)))
-          digits' (concat (rest digits) [(first digits)])]
+          digits' (digits-fn digits)]
         (->> [digits digits']
             (apply map vector)
             (filter (partial apply =))
             (map first)
             (reduce +))))
 
+(defn ^:private send-n-to-end [list n]
+    (concat (drop n list) (take n list)))
+
+;; 1228
+(defn step-1 []
+    (count-em #(send-n-to-end % 1)))
+
+;; 1238
+(defn step-2 []
+    (count-em #(send-n-to-end % (/ (count %) 2))))

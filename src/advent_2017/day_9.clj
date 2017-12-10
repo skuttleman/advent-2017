882da7e64aa1e83bad116d1d4cf82f0c4dd5fbd2
Ben Allred <skuttleman@gmail.com>
2017-12-10 09:49:29 -0700

    Day 9 Step 2

--- src/advent_2017/day_9.clj
+++ src/advent_2017/day_9.clj
@@ -4,39 +4,41 @@
 (defmulti ^:private determine-ctx (comp ffirst vector))
 
 (defmethod ^:private determine-ctx :normal
-    [[state nesting score :as ctx] char]
+    [[state nesting canceled score :as ctx] char]
     (case char
-        \{ [state (inc nesting) (+ score (inc nesting))]
-        \} [state (dec nesting) score]
-        \< [:garbage nesting score]
+        \{ [state (inc nesting) canceled (+ score (inc nesting))]
+        \} [state (dec nesting) canceled score]
+        \< [:garbage nesting canceled score]
         ctx))
 
 (defmethod ^:private determine-ctx :garbage
-    [[state nesting score :as ctx] char]
+    [[state nesting canceled score :as ctx] char]
     (case char
-        \! [:not nesting score]
-        \> [:normal nesting score]
-        ctx))
+        \! [:not nesting canceled score]
+        \> [:normal nesting canceled score]
+        [state nesting (inc canceled) score]))
 
 (defmethod ^:private determine-ctx :not
-    [[state nesting score :as ctx] char]
-    [:garbage nesting score])
+    [[state nesting canceled score :as ctx] char]
+    [:garbage nesting canceled score])
 
-(defn ^:private calc-score [[state nesting score :as ctx] [char & chars]]
+(defn ^:private calc-score [[state nesting canceled score :as ctx] [char & chars]]
     (let [ctx' (determine-ctx ctx char)]
         (if (empty? chars)
             ctx'
             (recur ctx' chars))))
 
 (defn ^:private prep []
-    (-> (u/read-file 9 #"\n")
-        (first)))
+    (->> (u/read-file 9 #"\n")
+        (first)
+        (calc-score [:normal 0 0 0])))
 
 ;; 12803
 (defn step-1 []
     (->> (prep)
-        (calc-score [:normal 0 0])
         (last)))
 
 (defn step-2 []
-    )
+    (->> (prep)
+        (butlast)
+        (last)))

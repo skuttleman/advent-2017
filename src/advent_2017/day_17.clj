6e1160d5756d86fab15dd295023effbbac7e2de2
Ben Allred <skuttleman@gmail.com>
2017-12-17 19:07:41 -0700

    Day 17 Step 2

--- src/advent_2017/day_17.clj
+++ src/advent_2017/day_17.clj
@@ -1,10 +1,10 @@
-(ns advent-2017.day-17)
+(ns advent-2017.day-17
+    (:require [advent-2017.utils.core :as u]))
 
+(defn ^:private step-forward [len pos steps]
+    (mod (+ pos steps) len))
 
-(defn step-forward [coll pos steps]
-    (mod (+ pos steps) (count coll)))
-
-(defn insert [coll pos value]
+(defn ^:private insert [coll pos value]
     (concat (take pos coll) [value] (drop pos coll)))
 
 ;; 136
@@ -12,14 +12,23 @@
     (let [steps 363 max-value 2017]
         (loop [coll [0] pos 0 next-value 1]
             (if (> next-value max-value)
-                (let [pos' (inc (step-forward coll pos (* -1 steps)))]
+                (let [pos' (inc (step-forward (count coll) pos (* -1 steps)))]
                     (first (drop pos' coll)))
                 (let [pos'  (inc pos)
                       coll' (insert coll pos' next-value)]
                     (recur coll'
-                        (step-forward coll' pos' steps)
+                        (step-forward (count coll') pos' steps)
                         (inc next-value)))))))
 
-
+;; 1080289
 (defn step-2 []
-    )
+    (let [steps 363 max-value 50000000]
+        (loop [second-val nil size 1 pos 0 next-value 1]
+            (if (> next-value max-value)
+                second-val
+                (let [size' (inc size)
+                      pos' (step-forward size' (inc pos) steps)]
+                    (recur (if (zero? pos) next-value second-val)
+                        (inc size)
+                        pos'
+                        (inc next-value)))))))
